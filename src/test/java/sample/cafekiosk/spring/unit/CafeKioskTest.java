efe5d352f1ac3c8b70b3bfc5ef88870cf836bddf
minturtle <minseok0425@kakao.com>
2023-07-03 14:50:28 +0900

    add 테스트 코드 작성

--- src/test/java/sample/cafekiosk/spring/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/spring/unit/CafeKioskTest.java
@@ -4,17 +4,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import sample.cafekiosk.spring.unit.beverages.Americano;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class CafeKioskTest {
 
 
     @Test
     @DisplayName("add Test")
     void t1() throws Exception {
-        final CafeKiosk cafeKiosk = new CafeKiosk();
+        CafeKiosk cafeKiosk = new CafeKiosk();
 
         cafeKiosk.add(new Americano());
 
-        System.out.println(cafeKiosk.getBeverages().size());
-        System.out.println(cafeKiosk.getBeverages().get(0).getName());
+        assertThat(cafeKiosk.getBeverages().size()).isEqualTo(1);
+        assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+
     }
 }
